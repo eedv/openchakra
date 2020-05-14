e5669a249d43edabf2c566b15f96d83bc19f36ce
--set <elfoton@gmail.com>
2020-05-14 20:49:22 -0300

    BREAKING CHANGE: remove chakra specific panels

--- src/components/inspector/panels/components/AlertIconPanel.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import TextControl from '../../controls/TextControl'
-
-const AlertIconPanel = () => {
-  return <TextControl name="size" label="Size" />
-}
-
-export default AlertIconPanel
